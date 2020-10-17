4025dc4d9d3e206f0ff6d15adc5cc744169fa79a
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-10-17 21:08:22 +0530

    Added some extra functionalities to app.js

--- express_app/app.js
+++ express_app/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+// the first matching route for any given link is considered
+// as the request to be rendered on a browser.
+
+
 app.get("/", function(req, res) {
     res.send("Hi!! Welocome to my new app");
 });
@@ -11,6 +15,15 @@ app.get("/doggy", function(req, res) {
 
 app.get("/bye", function(req, res) {
     res.send("Goodbye!! Have a good day!!");
+});
+
+// for creating any webpage with format /r/subreddit/commets/user
+// the following instance is used
+app.get("/r/:subreddit/comments/:user", function(req,res){
+    res.send("Hi Pranjal!!");
+    console.log(req.params);
+    console.log("Subreddit: "+req.params.subreddit.toUpperCase());
+    console.log("User: "+req.params.user.toUpperCase());
 });
 
 
