5eb1d593c26030cea73645affe9a4c484e1f2935
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2020-12-10 21:44:14 +0530

    Practice files added.

---
+++ long_text_assignment.php
@@ -0,0 +1,11 @@
+<?php
+    $channel = <<<_XML_
+     Hi! This is Pranjal Kumar.
+     It's nice to meet you all.
+    _XML_;
+    print($channel).PHP_EOL;
+    echo<<<END
+     It's nice to work with you guys,
+     hope will get the opportunity to grow.
+    END;
+?>
