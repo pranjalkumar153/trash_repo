2702cd3d5a3eeeb539dfb43950c9ef51d22a7bd9
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2020-12-10 21:31:42 +0530

    Basic practice with php

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php 
+    print "Hello World!". PHP_EOL;
+    print "Hello World!". PHP_EOL;
+    $name  = "Pranjal";
+    $literally = 'My $name will not print';
+    print($literally).PHP_EOL;
+    $literally = "My name: $name will print";
+    print($literally).PHP_EOL;
+?>
