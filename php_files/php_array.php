38b357e755bd1e1b08ea915753e9938ff9ef84c1
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2020-12-11 16:15:15 +0530

    Added array.

---
+++ php_files/php_array.php
@@ -0,0 +1,8 @@
+<?php
+    $array = array(1,2,3,4,5);
+    foreach($array as $value){
+        echo "The element accessed is $value".PHP_EOL;
+    }
+    $len = sizeof($array);
+    echo "$len".PHP_EOL;
+?>
